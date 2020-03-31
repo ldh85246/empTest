53b1a2f9b494087af0cee060b1d25ae8c80c88ef
종 호 <jj0101065@gmail.com>
2020-03-31 18:04:37 +0900

    종호 컨툴ㄹ러

--- empTest/src/main/java/com/example/demo/controller/DeptController.java
+++ empTest/src/main/java/com/example/demo/controller/DeptController.java
@@ -1,5 +1,38 @@
 package com.example.demo.controller;
 
-public class DeptController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dao.DeptDao;
+import com.example.demo.vo.DeptVo;
+import com.google.gson.Gson;
+
+@RestController
+public class DeptController {
+	@Autowired
+	private DeptDao dao;
+
+	public void setDao(DeptDao dao) {
+		this.dao = dao;
+	}
+	@RequestMapping("/listDept.do")
+	public String listDept() {
+		String str = "";
+		List<DeptVo>list = dao.listDept();
+		Gson gson = new Gson();
+		str = gson.toJson(list);
+		return str;
+	}
+	@RequestMapping("/insertDept.do")
+	public String insertDept(DeptVo d) {
+		int re = -1;
+		String str = "";
+		re = dao.insertDept(d);
+		Gson gson = new Gson();
+		str = gson.toJson(re);
+		return str;
+	}
 }
