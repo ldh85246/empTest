cecd3b2c36fe2915e33e859c96d773c4a33d8f73
park yu chul <pycupper@naver.com>
2020-03-31 17:54:52 +0900

    dao manager 완성

---
+++ empTest/src/main/java/com/example/demo/dao/DeptDao.java
@@ -0,0 +1,18 @@
+package com.example.demo.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.db.DeptManager;
+import com.example.demo.vo.DeptVo;
+
+@Repository
+public class DeptDao {
+	public List<DeptVo> listDept(){
+		return DeptManager.listDept();
+	}
+	public int insertDept(DeptVo d) {
+		return DeptManager.insertDept(d);
+	}
+}
---
+++ empTest/src/main/java/com/example/demo/dao/EmpDao.java
@@ -0,0 +1,24 @@
+package com.example.demo.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.db.EmpManager;
+import com.example.demo.vo.EmpVo;
+
+@Repository
+public class EmpDao {
+	public List<EmpVo> listEmp(){
+		return EmpManager.listEmp();
+	}
+	public int insertEmp(EmpVo e) {
+		return EmpManager.insertEmp(e);
+	}
+	public int updateEmp(EmpVo e) {
+		return EmpManager.updateEmp(e);
+	}
+	public int deleteEmp(EmpVo e) {
+		return EmpManager.deleteEmp(e);
+	}
+}
