7073cd60cf47a69c220f2b3a8df213930b323dd9
seodonguk1128 <seodonguk1128@gmail.com>
2020-03-31 17:26:59 +0900

    -서동욱

--- empTest/src/main/java/com/example/demo/vo/EmpVo.java
+++ empTest/src/main/java/com/example/demo/vo/EmpVo.java
@@ -3,29 +3,11 @@ package com.example.demo.vo;
 import java.util.Date;
 
 import lombok.AllArgsConstructor;
-<<<<<<< HEAD
-import lombok.NoArgsConstructor;
-import lombok.Data;
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class EmpVo {
-	private int eno;
-	private String ename;
-	private String job;
-	private int sal;
-	private int comm;
-	private int mgr;
-	private Date hiredate;
-	private int dno;
-	private String email;
-	private String jumin;
-=======
 import lombok.Data;
 import lombok.NoArgsConstructor;
-@AllArgsConstructor
-@NoArgsConstructor
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmpVo {
 	private int eno;
 	private String ename;
@@ -34,5 +16,5 @@ public class EmpVo {
 	private int comm;
 	private String hiredate;
 	private int dno;
->>>>>>> branch 'master' of https://github.com/ldh85246/empTest.git
+
 }
