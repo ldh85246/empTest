b7cdcb4c37582a780b1a18a99d3522a4e2dda974
seodonguk1128 <seodonguk1128@gmail.com>
2020-03-31 17:08:40 +0900

    서동욱

--- empTest/src/main/java/com/example/demo/vo/EmpVo.java
+++ empTest/src/main/java/com/example/demo/vo/EmpVo.java
@@ -1,5 +1,19 @@
 package com.example.demo.vo;
 
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class EmpVo {
-	
+	private int eno;
+	private String ename;
+	private String job;
+	private int sal;
+	private int comm;
+	private String hiredate;
+	private int dno;
 }
